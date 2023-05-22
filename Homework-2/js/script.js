c68b52b03848972c23360ef821729cd4ad48c867
Wiret02 <chuwachok6@gmail.com>
2023-05-23 00:42:00 +0300

    add 1, 2, 3, 5, 6 exercises

--- Homework-2/js/script.js
+++ Homework-2/js/script.js
@@ -1,13 +1,17 @@
-// Homework №2 //
+// // Homework №2 //
 
-// const a = prompt('type first number: '); // 1
+
+// // 1
+// const a = prompt('type first number: ');
 // const b = prompt('type second number: ');
 // const c = prompt('type third number: ');
 // const result = a < b && b < c;
 
 // alert("a < b < c ? This is: " + result);
 
-// let x = 1; // 2
+
+// // 2
+// let x = 1; 
 // let y = 2;
 
 // let res1 = String(x) + String(y);
@@ -26,6 +30,58 @@
 // console.log(res4); 
 // console.log(typeof res4);
 
-// const isAdult = prompt("How Old are you?") // 3
+
+// // 3
+// const isAdult = prompt("How Old are you?") 
 
 // isAdult >= 18 ? alert("Fine, you are adult!") : alert("Sorry, you are to young!");
+
+
+// 4
+
+
+
+// 5
+// const a = parseFloat(prompt("Type the length of first side of triangle"));
+// const b = parseFloat(prompt("Type the length of second side of triangle"));
+// const c = parseFloat(prompt("Type the length of third side of triangle"));
+
+// console.log(Math.pow(3, 2));
+// if ((a + b > c) && (a + c > b) && (b + c > a)) {
+//     if (Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)) === c) {
+//         alert('You have "right triangle"');
+//     } else {
+//         alert('Your have not "right triangle"');
+//     }
+//     const halfPerimeter = 0.5 * (a + b + c);
+//     const area = Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+
+//     alert("Area of your triangle is: " + area.toFixed(3))
+// } else {
+//     console.log("Incorrect data. The triangle with that sides is impossible");
+//     alert("Incorrect data. The triangle with that sides is impossible");
+// }
+
+
+//6
+// const a = parseFloat(prompt("Type the first number"));
+// const b = parseFloat(prompt("Type the second number"));
+// const op = parseFloat(prompt("Choose an operator:\n1. subtraction\n2. multiplication\n3. division\nother number - addition"));
+
+// function culc(a, b, op) {
+//     if (op === 1) {
+//         alert(a - b);
+//     } else if (op === 2) {
+//         alert(a * b);
+//     } else if (op === 3) {
+//         alert(a / b);
+//     } else {
+//         alert(a + b);
+//     }
+        
+// };
+
+// culc(a, b, op);
+
+
+// 7
