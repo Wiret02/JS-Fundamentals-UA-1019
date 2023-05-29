24faf4ea18e000382b2f8d8f263b502a61aa9c98
Wiret02 <chuwachok6@gmail.com>
2023-05-29 23:51:19 +0300

    add last 4 and 5 task to homework 4

--- Homework-4/js/script.js
+++ Homework-4/js/script.js
@@ -6,9 +6,9 @@
 // function sumSliceArray(arr, first, second) {
 //     try {
 //         if (typeof first !== 'number' || typeof second !== 'number') {
-//             throw TypeError('you put a data with wrong type, try again using only numbers for argument "first", "second"');
+//             throw new TypeError('you put a data with wrong type, try again using only numbers for argument "first", "second"');
 //         } else if (first > arr.length || second > arr.length) {
-//             throw RangeError('there is not any element in array with that index');
+//             throw new RangeError('there is not any element in array with that index');
 //         } else {
 //             return console.log(arr[first] + arr[second]);
 //         }
@@ -27,13 +27,13 @@
 //     const status = prompt('Type your status ("admin", "moderator", "user"):');
 
 //     if (name.length === 0 || status.length === 0) {
-//         throw Error('Acсess denied! \nThe field is empty! Please try again');
+//         throw new Error('Acсess denied! \nThe field is empty! Please try again');
 //     } else if (isNaN(age)) {
-//         throw EvalError('Acсess denied! \nWrong password! Please use numbers to type your age!');
+//         throw new EvalError('Acсess denied! \nWrong password! Please use numbers to type your age!');
 //     } else if (age < 18 || age > 70) {
-//         throw RangeError('Acсess denied! \nYour age is out of our rules');
+//         throw new RangeError('Acсess denied! \nYour age is out of our rules');
 //     } else if (status !== 'admin' && status !== 'moderator' && status !== 'user') {
-//         throw EvalError('Acсess denied! \n Wrong status! Please check and try again');
+//         throw new EvalError('Acсess denied! \n Wrong status! Please check and try again');
 //     } else {
 //         console.log('Success! \nChoose film to watch!');
 //     }
@@ -49,26 +49,69 @@
 // // 3
 // function calcRectangleArea(width, height) {
 //     if (typeof width !== 'number' || typeof height !== 'number') {
-//         throw TypeError('Wrong data type, please use numbers!');
+//         throw new TypeError('Wrong data type, please use numbers!');
 //     } else {
 //         return console.log('Rectangle Area is: ' + width * height);
 //     }
 // }
 
 // try {
-//     calcRectangleArea(7, 3)
+//     calcRectangleArea(7, 3);
 // } catch (exception) {
-//     console.log(exception.message)
+//     console.log(exception.message);
 // }
 
 
 // // 4
+// class MonthException extends Error {
+//     constructor(message) {
+//         super(message);
+//         this.name = "MonthException";
+//         this.message = message;
+//     }
+// }
 
+// function showMonthName(month) {
+//     try {
+//         const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+//         if (typeof month !== 'number' || month > 12 || month < 1) {
+//             throw new MonthException("Incorrect month number.");
+//         } else {
+//             console.log(months[month - 1]);
+//         }
+//     } catch (exception) {
+//         console.log(exception.stack);
+//     }
+
+// }
+
+// showMonthName(5);
+// showMonthName(14);
 
 
 // // 5
+function showUser(id) {
+    if (id < 0) {
+        throw new Error("ID must not be negative: " + id);
+    }
 
+    const idObj = {};
+    idObj.id = id;
+    return idObj;
+}
 
+function showUsers(ids) {
+    const idsArr = [];
+    for (let i = 0; i < ids.length; i++) {
+        try {
+            let id = showUser(ids[i]);
+            idsArr.push(id);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    return idsArr;
+}
 
-// // 6
-
+console.log(showUsers([7, -12, 44, 22]));
