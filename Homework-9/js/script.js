ab73c9a9bf7c4656935b4dc503514494e0c6f0f2
Wiret02 <chuwachok6@gmail.com>
2023-06-15 00:26:20 +0300

    homework-9 4/4

---
+++ Homework-9/js/script.js
@@ -0,0 +1,41 @@
+// Homework №9 //
+
+
+$(function () {
+
+
+    // // 1
+    // $('.head').css('backgroundColor', 'green');
+    // $('.head .inner').css('fontSize', '35px');
+
+
+    // // 2
+    // $link = $('a');
+    // if (/^https:\/\//.test($link.attr('href'))) {
+    //     $link.attr('target', '_blank');
+    // }
+
+
+    // // 3
+    // $div = $('div');
+    // $('body').prepend($div);
+
+
+    // // 4
+    // let $checkboxes = $('.checkbox');
+    // let counter = 0;
+    // $checkboxes.on('change', function () {
+    //     if (this.checked) {
+    //         counter++;
+    //         if (counter >= 3) {
+    //             setTimeout(function () {
+    //                 $checkboxes.prop('checked', false);
+    //                 counter = 0;
+    //             }, 500)
+    //         }
+    //     } else {
+    //         counter--;
+    //     }
+    // });
+
+});
